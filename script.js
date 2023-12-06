1f4c70af58c6c365fa4b70c3ef7ccd68134274b5
Deepak Kanshetti <kanshettid@gmail.com>
2023-12-06 21:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+function generateURL() {
+            // Get input values
+            var name = document.getElementById("name").value;
+            var year = document.getElementById("year").value;
+
+            // Update the URL
+            var urlString = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+            document.getElementById("url").textContent = urlString;
+        }
